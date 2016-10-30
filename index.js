343979176150289c9ed0b5b538920390681120c4
Konstantin Kirchev <konstantin1230@yahoo.com>
2016-10-30 14:09:49 -0400

    The environment and port are set

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const port = 1337
+const port = process.env.port || 1337
+const env = process.env.NODE_ENV || 'development'
 const connectionString = 'mongodb://localhost:27017/some-express-db'
 
 mongoose.Promise = global.Promise
